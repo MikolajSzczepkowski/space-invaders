0f961d0d548d34cc55fab89f1feddbd3badb1cca
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-07-13 14:40:48 +0200

    invaders attack

--- js/app.js
+++ js/app.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
   var $space = $(".space"),
       $area = $(".area"),
+      $inviders,
       moveInterval,
       blastInterval,
+      bombInterval,
       edgeLeft = false,
       edgeRight = false,
       currentX = 10,
       currentY = 19,
       blastX = currentX,
       blastY = 18,
+      bombX = 0,
+      bombY = 0,
       shooting = false,
       speed = 150;
+
+  // make user
+  coordinates(currentX, currentY).addClass("user");
+
+  // make inviders
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 20; j++) {
+      if (j%2 === 0) {
+        coordinates(j, i).addClass("invider")
+                          .attr("data-x", j)
+                          .attr("data-y", i);
+        $inviders = $(".invider");
+      }
+    }
+  }
+  // initiate invider attack
+  inviderAttack();
 
   function coordinates(x, y) {
       return $area.eq(x + (y * 20));
@@ -42,8 +63,20 @@ $(document).ready(function() {
     }
     shooting = true;
   }
+  function inviderAttack() {
+    var invider = $inviders[Math.floor(Math.random()*($inviders.length-1))];
+    bombX = parseInt($(invider).attr("data-x")),
+    bombY = parseInt($(invider).attr("data-y"));
+    coordinates(bombX, bombY).addClass("bomb");
+    bombInterval = setInterval(function() {
+      coordinates(bombX, bombY).toggleClass("bomb");
+      bombY++;
+      coordinates(bombX, bombY).toggleClass("bomb");
+      $($space).trigger("bombChanged");
+    }, speed);
+  }
   // check blast effect
-  $($space).bind("blastChanged" , function() {
+  $($space).bind("blastChanged", function() {
     if (blastY === 0) {
       shooting = false;
       window.clearInterval(blastInterval);
@@ -56,6 +89,19 @@ $(document).ready(function() {
       coordinates(blastX, blastY).toggleClass("blast");
       coordinates(blastX, blastY).toggleClass("invider");
       blastY = 18;
+    }
+  });
+  // check bomb effect
+  $($space).bind("bombChanged", function() {
+    if (bombY === 20) {
+      window.clearInterval(bombInterval);
+      coordinates(bombX, bombY).toggleClass("bomb");
+      inviderAttack();
+    }
+    if (coordinates(bombX, bombY).hasClass("user")) {
+      window.clearInterval(bombInterval);
+      coordinates(bombX, bombY).toggleClass("bomb");
+      coordinates(bombX, bombY).toggleClass("user");
     }
   });
   // check user move
@@ -86,15 +132,4 @@ $(document).ready(function() {
       }
   });
 
-// make user
-  coordinates(currentX, currentY).addClass("user");
-
-// make inviders
-  for (var i = 0; i < 4; i++) {
-    for (var j = 0; j < 20; j++) {
-      if (j%2 === 0) {
-        coordinates(j, i).addClass("invider");
-      }
-    }
-  }
 });
